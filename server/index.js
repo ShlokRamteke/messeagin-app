d296e5ab6443518e543f0f4b7cd8502b00850ddf
Shlok Ramteke <shlok.ramteke24@gmail.com>
2024-05-30 23:28:10 +0530

    Implemented pusher in backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import Cors from "cors";
+import Pusher from "pusher";
 dotenv.config();
 
 import Messages from "./Schema/dbMessages.js";
@@ -10,6 +11,14 @@ import Messages from "./Schema/dbMessages.js";
 const app = express();
 const port = process.env.PORT || 8000;
 const connection_url = process.env.DB_CONNECTION_URL;
+
+const pusher = new Pusher({
+  appId: "1810813",
+  key: "8942ae445a3177a54ae6",
+  secret: "4b12ce02ed029e2f9aca",
+  cluster: "ap2",
+  useTLS: true,
+});
 
 //Middlewares
 app.use(express.json());
@@ -26,16 +35,41 @@ mongoose
   .catch((err) => console.log(err.message));
 
 //API Endpoints
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("DB Connected");
+  const msgCollection = db.collection("messagingmessages");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log(change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("Error trigerring Pusher");
+    }
+  });
+});
+
 app.get("/", (req, res) => res.status(200).send("Hello World"));
 
 app.post("/messages/new", (req, res) => {
-  try {
-    const dbMessage = req.body;
-    res.status(201).send(dbMessage);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
+  const dbMessage = req.body;
+  Messages.create(dbMessage)
+    .then((dbMessage) => res.status(201).send("Data Sent"))
+    .catch((err) => {
+      // console.error(err);
+      res
+        .status(500)
+        .send({ message: "An error occurred while saving message" });
+    });
 });
 app.get("/messages/sync", (req, res) => {
   Messages.find()
